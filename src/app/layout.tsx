e71c1086b12dae96dbf5e5a92d581418efc66341
oriastanjung <oriastan999@gmail.com>
2024-03-09 02:44:19 +0700

    finish landing page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="font-sf-ui-medium container mx-auto px-4 relative top-0 text-black max-h-screen min-h-screen overflow-hidden">
+      <body className="font-sf-ui-medium container mx-auto px-4 relative top-0 text-black md:max-h-screen min-h-screen overflow-hidden">
         <Navbar />
         {children}
         <Footer/>
